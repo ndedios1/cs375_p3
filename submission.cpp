f4934b0b5528be3a8e15477ec75c01de3df43a27
Nickolas De Dios <ndedios1@cs.binghamton.edu>
2021-04-22 18:32:03 -0400

    algorithm recognition

--- submission.cpp
+++ submission.cpp
@@ -6,11 +6,27 @@
 #include <fstream>
 #include <sstream>
 #include <string>
+#include <cstring>
 
+using namespace std;
 
 int main(int argc, char* argv[]){
-
-
+	if(argc != 4){
+		cout << "insufficient args" << endl;
+	}
+	string algorithm_type = argv[3];
+	if(algorithm_type == "0"){
+		cout << "using greedy algorithm 1" << endl;
+	}
+	else if(algorithm_type == "1"){
+		cout << "using greedy algorithm 2" << endl;
+	}
+	else if(algorithm_type == "2"){
+		cout << "using backtracking algorithm" << endl;
+	}
+	else{
+		cout << "third value has incorrect values" << endl;
+	}
 
 
 
